ad8ed781962f1885101a4b43539ad1450b65bf6c
João <105893644+JotaVSRodrigues@users.noreply.github.com>
2025-08-02 16:53:50 -0300

    Add files via upload

---
+++ JavaDB-main/BancoDeDados/src/main/java/forms/TelaCadastro.java
@@ -0,0 +1,171 @@
+package forms;
+
+import javax.swing.JOptionPane;
+import src.AuthService;
+
+public class TelaCadastro extends javax.swing.JFrame {
+    
+    AuthService autentitacao = new AuthService();
+    
+    public TelaCadastro() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        fieldEmail = new javax.swing.JTextField();
+        fieldNome = new javax.swing.JTextField();
+        fieldSenha = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("Cadastro de Usuário");
+
+        jLabel2.setText("Nome:");
+
+        jLabel3.setText("Email:");
+
+        jLabel4.setText("Senha:");
+
+        fieldNome.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                fieldNomeActionPerformed(evt);
+            }
+        });
+
+        jButton1.setText("Cadastro");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(87, 87, 87)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jButton1)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(jLabel3)
+                                    .addComponent(jLabel4)
+                                    .addComponent(jLabel2))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(fieldNome, javax.swing.GroupLayout.DEFAULT_SIZE, 160, Short.MAX_VALUE)
+                                    .addComponent(fieldEmail)
+                                    .addComponent(fieldSenha))))))
+                .addContainerGap(105, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addGap(47, 47, 47)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(fieldNome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(8, 8, 8)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(fieldEmail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(fieldSenha, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton1)
+                .addContainerGap(100, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void fieldNomeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_fieldNomeActionPerformed
+        
+    }//GEN-LAST:event_fieldNomeActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        try {
+            String nome = fieldNome.getText();
+            String email = fieldEmail.getText();
+            String senha = fieldSenha.getText();
+
+            AuthService auth = new AuthService();
+
+            if(auth.registerUser(nome, email, senha)){
+                JOptionPane.showMessageDialog(this, "Cadastro realizado com sucesso" +
+                " Nome: " + nome +     
+                " Email: " + email +
+                "Senha: " + senha
+                );
+            } else {
+                JOptionPane.showMessageDialog(null, "Email ou senha inválidos");
+            }
+        } catch(Exception ex){
+            JOptionPane.showMessageDialog(this, "houve um erro: " + ex);
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(TelaCadastro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(TelaCadastro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(TelaCadastro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(TelaCadastro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new TelaCadastro().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField fieldEmail;
+    private javax.swing.JTextField fieldNome;
+    private javax.swing.JTextField fieldSenha;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ JavaDB-main/BancoDeDados/src/main/java/src/AuthService.java
@@ -0,0 +1,93 @@
+package src;
+
+import java.sql.*;
+import org.mindrot.jbcrypt.BCrypt;
+
+public class AuthService {
+    public boolean registerUser(String nome, String email, String senha){
+        // cria uma variável que armazena a query que será utilizada na criação de um
+        // objeto na tabela users
+        String query = "INSERT INTO users (nome, email, senha) VALUES (?,?,?)";
+        String senhaHash = BCrypt.hashpw(senha, BCrypt.gensalt());
+        
+        // Connection -> classe que cria um objeto conector ao banco de dados
+        // PreparedStatement -> executar as consultas no banco
+        try (Connection conexao = DBconnection.getConnection();
+            PreparedStatement objeto = conexao.prepareStatement(query)) {
+            
+            objeto.setString(1, nome);
+            objeto.setString(2, email);
+            objeto.setString(3, senhaHash);
+            
+            /* executando o cadastro do usuário e retornando sucesso ou falha*/
+            
+            objeto.executeUpdate();
+            return true;
+
+        } catch (SQLException ex) {
+           ex.printStackTrace();
+           return false;
+        }
+    }
+    
+    public boolean loginUser(String email, String senha) throws SQLException{
+        String query = "SELECT * FROM users WHERE email = ?";
+        
+        try (Connection conexao = DBconnection.getConnection();
+             PreparedStatement login = conexao.prepareStatement(query)){
+            
+            login.setString(1, email);
+           
+            // ResultSet -> Lib que permite realizar consultas no banco
+            // Nesse trecho, criamos um objeto para consultar o banco de busca do email solicitado e executamos a busca
+            ResultSet consulta = login.executeQuery();
+            
+            if (consulta.next()) {
+                String senhaArm = consulta.getString("senha");
+                
+                if (BCrypt.checkpw(senha, senhaArm)) {
+                    return true;
+                }
+            } return false;
+        } catch (SQLException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+    
+    // Passo 1 -> verificar se o usuário existe a partir do email
+    public boolean verificarEmail(String email){
+        String query = "SELECT * FROM users WHERE email = ?";
+        
+        try (Connection connection = DBconnection.getConnection();
+            PreparedStatement consulta = connection.prepareStatement(query)){
+            
+            consulta.setString(1, email);
+            
+            ResultSet resultado = consulta.executeQuery();
+            return resultado.next();
+            
+        } catch (SQLException ex){
+            ex.printStackTrace();
+            return false;
+        }
+    }
+    public boolean atualizarSenha (String email, String novaSenha){
+        String query = "UPDATE users SET senha = ? WHERE email = ?";
+        
+        try (Connection connection = DBconnection.getConnection();
+            PreparedStatement consulta = connection.prepareStatement(query)){
+            
+            consulta.setString(1, email);
+            consulta.setString(2, novaSenha);
+            
+            int linhasAlteradas = consulta.executeUpdate();
+            return linhasAlteradas > 0;
+            
+        } catch (SQLException ex){
+            ex.printStackTrace();
+            return false;
+        }
+    }
+}
+
